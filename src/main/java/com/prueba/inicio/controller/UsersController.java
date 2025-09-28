e6838f32d776894f893d0f07957caaf133ad3368
mamunoz <mamunozlob@gmail.com>
2025-09-28 11:13:10 -0600

    Cambio de controller

--- src/main/java/com/prueba/inicio/controller/UsersController.java
+++ src/main/java/com/prueba/inicio/controller/UsersController.java
@@ -14,12 +14,14 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prueba.inicio.model.User;
 import com.prueba.inicio.service.UserService;
 @RestController
+@RequestMapping("/user")
 public class UsersController {
 
 	private final UserService service = new UserService();
